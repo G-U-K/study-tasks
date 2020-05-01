85eb280615ee986a48e6b487b52f3c6016bac959
G-U-K <36925748+G-U-K@users.noreply.github.com>
2020-05-01 19:38:04 +0700

    ВЫПОЛНЕНО А/ ТИПС ЛР6 черновая/02.05

---
+++ matlab/ТИПС/ЛАБ 6/IPST_lab6_various.m
@@ -0,0 +1,20 @@
+function count = IPST_lab6_various(vector)
+% возвращает количество различных(отличающихся) чисел в векторе
+
+% если входной арг это матрица
+if(~isvector(vector))
+    disp('Входной аргумент должен быть вектором/скаляром!')
+    return
+end
+
+count=0;
+
+% пока в векторе остаются числа
+while(~isempty(vector))
+%     исключаем из вектора числа
+%     которые равны первому
+%     vector = vector(abs(1 - vector./vector(1))<eps.*10); альтернативное
+    vector = vector(vector~=vector(1));
+    count = count+1;
+end
+end
